b60eef407aff88e4a32b999f786fecfbe9f96346
rramirez <rramirez@capmega.com>
2019-06-20 12:58:32 -0500

    Creacion de dos nuevos archivos; la ventana y inicial y su clase, ademas de agregarlo al android manifest

---
+++ app/src/main/java/google/rrp/capmegakotlin/EjercicioOne.kt
@@ -0,0 +1,18 @@
+package google.rrp.capmegakotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * Jun 2019
+ * Raúl Ramírez
+ * rramirez@capmega.com
+ */
+class EjercicioOne : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ejercicio_one)
+    }
+}
--- app/src/main/java/google/rrp/capmegakotlin/MainActivity.kt
+++ app/src/main/java/google/rrp/capmegakotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package google.rrp.capmegakotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
