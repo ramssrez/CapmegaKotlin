0b856272391c6b5344e4345e966a9ab340f39278
rramirez <rramirez@capmega.com>
2019-06-21 13:06:46 -0500

    Ya se observa el layaout de la suma con el evento mandado a llamar desde el layout del main activity

--- app/src/main/java/google/rrp/capmegakotlin/MainActivity.kt
+++ app/src/main/java/google/rrp/capmegakotlin/MainActivity.kt
@@ -3,11 +3,18 @@ package google.rrp.capmegakotlin
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun goToEjercicioOne(view: View) {
+        val intent = Intent(this, EjercicioOne::class.java)
+        startActivity(intent)
+        finish()
     }
 }
