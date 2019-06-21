fa216276cb60569343dcc3d92ec898b0d325e7a1
rramirez <rramirez@capmega.com>
2019-06-21 13:21:40 -0500

    Funcionalidad del segundo boton

--- app/src/main/java/google/rrp/capmegakotlin/EjercicioOne.kt
+++ app/src/main/java/google/rrp/capmegakotlin/EjercicioOne.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.ejercicio_one.*
 
 /*
  * Capmega Soluciones Informáticas S.A.S de C.V
@@ -11,12 +12,19 @@ import android.widget.Toast
  * Raúl Ramírez
  * rramirez@capmega.com
  */
-class EjercicioOne : AppCompatActivity() {
+class EjercicioOne : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ejercicio_one)
+        btn_calcular_enciso_c.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(applicationContext, "Prueba de impresion de pantalla del tercer  boton ", Toast.LENGTH_SHORT).show()
+
+    }
+
 
     fun suma(view : View){
         Toast.makeText(applicationContext, "Prueba de impresion de pantalla del primer boton ", Toast.LENGTH_SHORT).show()
