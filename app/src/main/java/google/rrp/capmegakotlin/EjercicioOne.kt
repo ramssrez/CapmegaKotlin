34df3bdf72a0625549dec02fa9945c2b80deaa9e
rramirez <rramirez@capmega.com>
2019-06-21 13:13:26 -0500

    Funcionalidad del primer boton

--- app/src/main/java/google/rrp/capmegakotlin/EjercicioOne.kt
+++ app/src/main/java/google/rrp/capmegakotlin/EjercicioOne.kt
@@ -2,6 +2,8 @@ package google.rrp.capmegakotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 
 /*
  * Capmega Soluciones Informáticas S.A.S de C.V
@@ -14,5 +16,9 @@ class EjercicioOne : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ejercicio_one)
+    }
+
+    fun suma(view : View){
+        Toast.makeText(applicationContext, "Prueba de impresion de pantalla del primer boton ", Toast.LENGTH_SHORT).show()
     }
 }
