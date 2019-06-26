e188220c042dcea5c9057831248a275bdb8ceb39
rramirez <rramirez@capmega.com>
2019-06-26 11:36:06 -0500

    nada interesante

---
+++ app/src/main/java/google/rrp/capmegakotlin/EjercicioTwo.kt
@@ -0,0 +1,21 @@
+package google.rrp.capmegakotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * Jun 2019
+ * Raúl Ramírez
+ * rramirez@capmega.com
+ */
+class EjercicioTwo : AppCompatActivity(), View.OnClickListener {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ejercicio_two)
+    }
+}
