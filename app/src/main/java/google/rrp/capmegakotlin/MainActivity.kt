121e8d7bc6711452c11837cd0ea0b940c78b155a
rramirez <rramirez@capmega.com>
2019-06-26 11:31:22 -0500

    Guardado de cambios menores

--- app/src/main/java/google/rrp/capmegakotlin/MainActivity.kt
+++ app/src/main/java/google/rrp/capmegakotlin/MainActivity.kt
@@ -1,12 +1,19 @@
 package google.rrp.capmegakotlin
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun go_to_ejercicio_two(view : View){
+        val intent = Intent(this, EjercicioTwo::class.java)
+        startActivity(intent)
+        finish()
     }
 }
