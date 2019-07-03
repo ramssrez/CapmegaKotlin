264513d0549f714b47c6c4fdd1ba948845b7169d
rramirez <rramirez@capmega.com>
2019-07-03 12:15:26 -0500

    Mejora del la clase del activity main

--- app/src/main/java/google/rrp/capmegakotlin/MainActivity.kt
+++ app/src/main/java/google/rrp/capmegakotlin/MainActivity.kt
@@ -4,45 +4,60 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_go_ejercicio_one.setOnClickListener(this)
+        btn_go_ejercicio_two.setOnClickListener(this)
+        btn_go_ejercicio_three.setOnClickListener(this)
+        btn_go_ejercicio_four.setOnClickListener(this)
+        btn_go_ejercicio_six.setOnClickListener(this)
+        btn_go_ejercicio_seven.setOnClickListener(this)
     }
 
-    fun goToEjercicioOne(view: View) {
-        val intent = Intent(this, EjercicioOne::class.java)
-        startActivity(intent)
-        finish()
+
+    override fun onClick(v: View?) {
+        when (v?.id){
+            R.id.btn_go_ejercicio_one ->{
+                val intent = Intent(this, EjercicioOne::class.java)
+                startActivity(intent)
+                finish()
+            }
+            R.id.btn_go_ejercicio_two ->{
+                val intent = Intent(this, EjercicioTwo::class.java)
+                startActivity(intent)
+                finish()
+            }
+            R.id.btn_go_ejercicio_three ->{
+                val intent = Intent(this, EjercicioThree::class.java)
+                startActivity(intent)
+                finish()
+            }
+            R.id.btn_go_ejercicio_four ->{
+                val intent = Intent(this, EjercicioFour::class.java)
+                startActivity(intent)
+                finish()
+            }
+            R.id.btn_go_ejercicio_six ->{
+                val intent = Intent(this, EjercicioSix::class.java)
+                startActivity(intent)
+                finish()
+            }
+            R.id.btn_go_ejercicio_seven ->{
+                val intent = Intent(this, EjercicioSeven::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+
     }
 
-    fun go_to_ejercicio_two(view: View) {
-        val intent = Intent(this, EjercicioTwo::class.java)
-        startActivity(intent)
-        finish()
-    }
-    fun go_to_ejercicio_three(view: View) {
-        val intent = Intent(this, EjercicioThree::class.java)
-        startActivity(intent)
-        finish()
-    }
 
-    fun go_to_ejercicio_four (view : View){
-        val intent = Intent(this, EjercicioFour::class.java)
-        startActivity(intent)
-        finish()
-    }
-    fun go_to_ejercicio_six(view: View) {
-        val intent = Intent(this, EjercicioSix::class.java)
-        startActivity(intent)
-        finish()
-    }
 
-    fun go_to_ejercicio_seven(view: View) {
-        val intent = Intent(this, EjercicioSeven::class.java)
-        startActivity(intent)
-        finish()
-    }
 }
