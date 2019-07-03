6cafe776dbadd9fbbd8d046c563bb271df86e8af
rramirez <rramirez@capmega.com>
2019-07-03 11:38:28 -0500

    Creacion de los archivos para el ejercicio 7 y la vinculacion con el activity main

---
+++ app/src/main/java/google/rrp/capmegakotlin/EjercicioSeven.kt
@@ -0,0 +1,22 @@
+package google.rrp.capmegakotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+/*
+ * Capmega Soluciones Informáticas S.A.S de C.V
+ * Jul 2019
+ * Raúl Ramírez
+ * rramirez@capmega.com
+ */
+class EjercicioSeven : AppCompatActivity() , View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.ejercicio_seven)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
